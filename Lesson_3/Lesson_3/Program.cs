190fa2511f5021e3322ae759a776f3739b04a8c0
Cyc <chernous.alex@gmail.com>
2021-04-29 01:39:56 +0300

    Completed_HM3

--- Lesson_3/Lesson_3/Program.cs
+++ Lesson_3/Lesson_3/Program.cs
@@ -7,19 +7,30 @@ namespace Lesson_3
 {
     class Program
     {
-        public PointClass[] arrayClass = GenPointClass();
-        public PointStruct[] arrayStruct = GenPointStruct();
         static void Main(string[] args)
         {
             BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
             Console.ReadKey();
         }
+    }
+    public class PointClass
+    {
+        public int _x;
+        public int _y;
+    }
 
+    public struct PointStruct
+    {
+        public int _x;
+        public int _y;
+    }
+    public class BenchmarkClass
+    {
         public static PointClass[] GenPointClass()
         {
-            //Random rnd = new Random();
-            var point_1 = new PointClass() { _x = 21, _y = 15 };
-            var point_2 = new PointClass() { _x = 13, _y = 5 };
+            Random rnd = new Random();
+            var point_1 = new PointClass() { _x = rnd.Next(0, 50), _y = rnd.Next(0, 50) };
+            var point_2 = new PointClass() { _x = rnd.Next(0, 50), _y = rnd.Next(0, 50) };
             var point_array = new PointClass[2];
             point_array[0] = point_1;
             point_array[1] = point_2;
@@ -35,64 +46,57 @@ namespace Lesson_3
             point_array[1] = point_2;
             return point_array;
         }
-        public class PointClass
+        public PointClass[] arrayClass = GenPointClass();
+        public PointStruct[] arrayStruct = GenPointStruct();
+        public float PointDist_ClassFloat(PointClass[] arrayClass)
         {
-            public int _x;
-            public int _y;
-        }
-
-        public struct PointStruct
-        {
-            public int _x;
-            public int _y;
-        }
-
-        public float PointDist_ClassFloat(PointClass[] pointArray)
-        {
-            float x = pointArray[0]._x - pointArray[1]._x;
-            float y = pointArray[0]._y - pointArray[1]._y;
+            float x = arrayClass[0]._x - arrayClass[1]._x;
+            float y = arrayClass[0]._y - arrayClass[1]._y;
             return MathF.Sqrt((x * x) + (y * y));
         }
 
-        public float PointDist_StructFloat(PointStruct[] pointArray)
+        public float PointDist_StructFloat(PointStruct[] arrayStruct)
         {
-            float x = pointArray[0]._x - pointArray[1]._x;
-            float y = pointArray[0]._y - pointArray[1]._y;
+            float x = arrayStruct[0]._x - arrayStruct[1]._x;
+            float y = arrayStruct[0]._y - arrayStruct[1]._y;
             return MathF.Sqrt((x * x) + (y * y));
         }
 
-        public double PointDist_StructDouble(PointStruct[] pointArray)
+        public double PointDist_StructDouble(PointStruct[] arrayStruct)
         {
-            double x = pointArray[0]._x - pointArray[1]._x;
-            double y = pointArray[0]._y - pointArray[1]._y;
+            double x = arrayStruct[0]._x - arrayStruct[1]._x;
+            double y = arrayStruct[0]._y - arrayStruct[1]._y;
             return Math.Sqrt((x * x) + (y * y));
         }
 
-        public float PointDistShort_StructFloat(PointStruct[] pointArray)
+        public float PointDistShort_StructFloat(PointStruct[] arrayStruct)
         {
-            float x = pointArray[0]._x - pointArray[1]._x;
-            float y = pointArray[0]._y - pointArray[1]._y;
+            float x = arrayStruct[0]._x - arrayStruct[1]._x;
+            float y = arrayStruct[0]._y - arrayStruct[1]._y;
             return (x * x) + (y * y);
         }
-        //[Benchmark]
-        //public void Test_ClassFloat(PointClass[] arrayClass)
-        //{
-        //    PointDist_ClassFloat(arrayClass);
-        //}
         [Benchmark]
-        public void Test_StructFloat(PointStruct[] arrayStruct)
+        public void Test_ClassFloat()
+        {
+            PointDist_ClassFloat(arrayClass);
+        }
+        [Benchmark]
+        public void Test_StructFloat()
         {
             PointDist_StructFloat(arrayStruct);
         }
         [Benchmark]
-        public void Test_StructDouble(PointStruct[] arrayStruct)
+        public void Test_StructDouble()
         {
             PointDist_StructDouble(arrayStruct);
         }
         [Benchmark]
-        public void Test_StructFloatShort(PointStruct[] arrayStruct)
+        public void Test_StructFloatShort()
         {
             PointDistShort_StructFloat(arrayStruct);
         }
     }
 }
+
+
+
