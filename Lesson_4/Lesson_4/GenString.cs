a0b3f8ca8fb38e63808f0f6bbba9966f8c8e0824
Cyc <chernous.alex@gmail.com>
2021-04-29 19:59:05 +0300

    HM4_InProgress

---
+++ Lesson_4/Lesson_4/GenString.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_4
+{
+    public class GenString
+    {
+        public static string GenericString(string set, int lenght)
+        {
+            Random rnd = new Random();
+            StringBuilder sb = new StringBuilder(lenght - 1);
+            int pos;
+            for (int i = 0; i < lenght; i++)
+            {
+                pos = rnd.Next(0, set.Length - 1);
+                sb.Append(set[pos]);
+            }
+            return sb.ToString();
+        }
+        public static List<string> ListString(int num, string set, int lenght)
+        {
+            List<string> stringList = new List<string>();
+            for (int i = 0; i < num; i++)
+            {
+                string newString = GenericString(set, lenght);
+                stringList.Add(newString);
+            }
+            return stringList;
+        }
+        //public static void DisplayString(List<string> strList)
+        //{
+        //    for(int i = 0; i < strList.Count; i++)
+        //    {
+        //        Console.WriteLine($"{strList[i]}\n\r");
+        //    }
+        //}
+    }
+}
---
+++ Lesson_4/Lesson_4/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Lesson_4
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GenString.DisplayString(strList);
+
+            //TestSearch.ArraySearch(strList);
+            //TestSearch.HashSetSearch(strList);
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+            Console.ReadKey();
+        }
+    }
+    public class TestSearch
+    {
+        public string RandomString { get; set; }
+        public List<string> strList = GenString.ListString(100, "qwertyuiopasdfghjklzxcvbnm1029384756", 6);
+        //public HashSet<TestSearch> hashSet;
+        public override bool Equals(object obj)
+        {
+            var user = obj as TestSearch;
+
+            if (user == null)
+                return false;
+
+            return RandomString == user.RandomString;
+        }
+        public override int GetHashCode()
+        {
+            int RandomStringCode = RandomString?.GetHashCode() ?? 0;
+            return RandomStringCode;
+        }
+        //public TestSearch ListToHashSet(List<string> strList)
+        //{
+        //    var hashSet = new HashSet<TestSearch>();
+        //    for (int i = 0; i < strList.Count; i++)
+        //    {
+        //        var strHash = new TestSearch() { RandomString = strList[i] };
+        //        hashSet.Add(strHash);
+        //    }
+        //    return hashSet;
+        //}
+    
+
+        [Benchmark]
+        public void HashSetSearch()
+        {
+            Random rnd = new Random();
+            var hashSet = new HashSet<TestSearch>();
+            for (int i = 0; i < strList.Count; i++)
+            {
+                var strHash = new TestSearch() { RandomString = strList[i] };
+                hashSet.Add(strHash);
+            }
+            var rndString = new TestSearch() { RandomString = strList[rnd.Next(0, 100)] };
+            Console.WriteLine($"contains string {hashSet.Contains(rndString)}, {rndString.RandomString}");
+        }
+        [Benchmark]
+        public void ArraySearch()
+        {
+            var result = false;
+            Random rnd = new Random();
+            int k = strList.Count;
+            string[] arrayString = new string[k];
+            for (int i = 0; i < strList.Count; i++)
+            {
+                arrayString[i] = strList[i];
+            }
+            var rndString = arrayString[rnd.Next(0, 100)];
+            foreach (var str in arrayString)
+            {
+                if (str == rndString)
+                {
+                    result = true;
+                    Console.WriteLine($"contains string {result}, {str}");
+                }
+                else
+                {
+                    Console.WriteLine($"contains string {result}, {str}");
+                }
+            }
+
+        }
+    }
+}
